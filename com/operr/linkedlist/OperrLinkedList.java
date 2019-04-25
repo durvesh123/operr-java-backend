6d205a35b1d2d8c1d65142c2c83a44c8842e3b21
Durvesh Vedak <durveshvedak@Durveshs-MBP.fios-router.home>
2019-04-24 22:01:24 -0400

    * implemented the removeNodesGreaterThanValue method

--- com/operr/linkedlist/OperrLinkedList.java
+++ com/operr/linkedlist/OperrLinkedList.java
@@ -31,8 +31,28 @@ public class OperrLinkedList {
 
     }
 
-    public void removeNodesGreaterThanValue(int val) {
+    public void removeNodesGreaterThanValue(int value) {
+        if (head == null) {
+            return;
+        }
+        Node temp = head, prev;
+        // removing all head nodes having val > value
+        while (temp.getNext() != null && temp.getVal() > value) {
+            head = temp.getNext();
+            temp = head;
+        }
+        // removal of remaining nodes
+        while (temp.getNext() != NULL) {
+            while (temp.getNext() != null && temp.getVal() <= value) {
+                prev = temp;
+                temp = temp.getNext();
+            }
+            if (temp == NULL)
+                return;
 
+            prev.setNext(temp.getNext());
+            temp = prev.getNext();
+        }
     }
 
 }
